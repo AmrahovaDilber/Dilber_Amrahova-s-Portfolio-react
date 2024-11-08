a7878b08d284b2d7b6803149bc727b441b0e8d97
AmrahovaDilber <amrahovad911@gmail.com>
2024-11-08 15:09:04 +0400

    added DownloadCv section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,34 +2,33 @@ import PhotoBox from "../components/PhotoBox";
 import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 import "../assets/styles/Home.scss";
-
+import DownloadCv from "../components/DownloadCv";
 
 const Home = () => {
   const navigate = useNavigate();
-  
+
   const handleKnowMoreClick = () => {
     navigate("/inner");
-  }
-
-  
+  };
 
   return (
     <section className="home">
-      <div className="home__photobox">
-        <div className="home_container">
-          <PhotoBox
-            name="Dilbər Əmrahova"
-            title="Programmer. Creative. Innovator"
-            className="myimage"
-            className2="photo-box__class1"
-            description="My journey in Information Technology is fueled by curiosity and a love for creative coding. Curious to know more? Click below!"
-          />
+      <div className="home__overlay"></div>
+      <div className="home_container">
+        <PhotoBox
+          name="Dilbər Əmrahova"
+          title="Programmer. Creative. Innovator"
+          className="myimage"
+          className2="photo-box__class1"
+          description="My journey in Information Technology is fueled by curiosity and a love for creative coding. Curious to know more? Click below!"
+        />
+        <div className="home-buttons">
           <Button
             className="home-button"
             text="Know More"
             onClick={handleKnowMoreClick}
           />
-          {/* <DownloadCv></DownloadCv> */}
+          <DownloadCv></DownloadCv>
         </div>
       </div>
     </section>
